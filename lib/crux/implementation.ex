52b58d91d9db848f3ede8e9947b0f50727d49e93
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-01 17:54:36 +0100

    chore: SPDX contributor links (#14)

--- lib/crux/implementation.ex
+++ lib/crux/implementation.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 crux contributors <https://github.com/ash-project/crux/graphs.contributors>
+# SPDX-FileCopyrightText: 2025 crux contributors <https://github.com/ash-project/crux/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
